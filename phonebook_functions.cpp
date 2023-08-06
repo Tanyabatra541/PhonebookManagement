83c6966e4260391f40cffbc8e1dbed09361ad807
Varun <62049199+varuuntiwari@users.noreply.github.com>
2023-08-06 23:32:20 +0530

    added sqlite3 lib, add/delete working

--- phonebook_functions.cpp
+++ phonebook_functions.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <fstream>
 #include <cstdio>
+#include <sqlite3.h>
+#define DB_NAME "contacts.sqlite"
 
 using namespace std;
 
@@ -18,6 +20,7 @@ struct phonebook ph;
 const int MAX_CONTACTS = 100;
 vector<phonebook> contacts_array;
 char phonebook_file[] = "phonebook.txt";
+sqlite3* db;
 
 bool read_contacts() {
     ifstream fin(phonebook_file);
@@ -35,31 +38,50 @@ bool read_contacts() {
 }
 
 void add_contact() {
-    if(contacts_array.size() <= MAX_CONTACTS){
+    if(contacts_array.size() <= MAX_CONTACTS) {
         cout<<"Enter the name to be added: ";
         getline(cin, ph.name);
         cout<<"Enter the number: ";
         getline(cin,ph.number);
+
+        // For an array
         contacts_array.push_back({ph.name, ph.number});
         
-        //for a file
+        // For a file
         ofstream fout(phonebook_file, ios::app); //ios is for input output stream, can use ofstream here too
         // ofstream fout(phonebook_file, ios::out | ios::trunc); This is to clear the file contents
         fout<<ph.name<<","<<ph.number<<"\n";
         fout.close();
+
+        // For a database
+        sqlite3* db;
+        string insertRecordQuery = "INSERT INTO contacts (name, number) VALUES (' "+ ph.name +"', '"+ ph.number +"');";
+        char* errorMsg;
+        // Save the connection result
+        int rc = sqlite3_open(DB_NAME, &db); 
+
+        // Run commands on DB
+        rc = sqlite3_exec(db, insertRecordQuery.c_str(), 0, 0, &errorMsg);
+        if (rc != SQLITE_OK) {
+            cerr<<"\033[31mSQL error: "<< errorMsg<<"\033[0m"<<endl;
+            sqlite3_free(errorMsg);
+        }
+
+        // Close the connection
+        sqlite3_close(db);
         cout<<"\n\033[32mContact added!!\033[0m\n";
     }
-    else{
+    else {
         cout<<"Not enough memory. Delete some contacts";
     }
 }
 
 void delete_contact() {
     string toBeDeleted;
-    cout<<"Enter the name or number to be deleted: ";
+    cout<<"Enter the number to be deleted: ";
     getline(cin, toBeDeleted);
     for(auto i = contacts_array.begin(); i != contacts_array.end(); i++){
-        if(i -> name == toBeDeleted || i -> number == toBeDeleted){
+        if(i -> number == toBeDeleted){
             contacts_array.erase(i);
             break;
         }
@@ -78,22 +100,36 @@ void delete_contact() {
     fin.close();
     remove(phonebook_file);
     rename("temp.txt", phonebook_file);
- 
-    cout<<"\n\033[31mContact Deleted!!\033[0m\n";
+
+    // For a database
+    sqlite3* db;
+    string delRecordQuery = "DELETE FROM contacts WHERE number='"+ toBeDeleted +"';";
+    char* errorMsg;
+    // Save the connection result
+    int rc = sqlite3_open(DB_NAME, &db);
+    // Run commands on DB
+    rc = sqlite3_exec(db, delRecordQuery.c_str(), 0, 0, &errorMsg);
+    if (rc != SQLITE_OK) {
+        cerr<<"\033[31mSQL error: "<< errorMsg<<"\033[0m"<<endl;
+        sqlite3_free(errorMsg);
+    } else {
+        cout<<"\n\033[31mContact Deleted!!\033[0m\n";
+    }
+    // Close the connection
+    sqlite3_close(db);
 }
 
 void modify_contact() {
     string toBeModified;
-    cout<<"Enter the name or number to be deleted: "<<endl;
+    cout<<"Enter the name or number to be deleted: ";
     getline(cin, toBeModified);
     string modifiedNumber;
     cout<<"Enter the modified number: ";
     getline(cin, modifiedNumber);
-    for(auto i=contacts_array.begin();i!=contacts_array.end();i++){
-        if(i -> number == toBeModified || i -> name == toBeModified){
+    for(auto i=contacts_array.begin(); i!=contacts_array.end(); i++) {
+        if(i -> number == toBeModified || i -> name == toBeModified) {
             i -> number = modifiedNumber;
         }
-        
     }
 
     // for a file
@@ -103,8 +139,8 @@ void modify_contact() {
     while (getline(fin, line)){
         stringstream ss(line);
         string str1, str2;
-        if (getline(ss, str1, ',') && getline(ss, str2, ',')){
-            if(str1 == toBeModified || str2 == toBeModified){
+        if (getline(ss, str1, ',') && getline(ss, str2, ',')) {
+            if(str1 == toBeModified || str2 == toBeModified) {
                 str2 = modifiedNumber;
                 fout<<str1<<","<<str2<<"\n";
             }
@@ -118,7 +154,22 @@ void modify_contact() {
     remove(phonebook_file);
     rename("temp.txt", phonebook_file);
 
-    cout<<"\n\033[33mContact Modified!!\033[0m\n";
+    // For a database
+    sqlite3* db;
+    string modRecordQuery = "UPDATE contacts SET number='"+ modifiedNumber +"' WHERE name='" + toBeModified + "';";
+    char* errorMsg;
+    // Save the connection result
+    int rc = sqlite3_open(DB_NAME, &db);
+    // Run commands on DB
+    rc = sqlite3_exec(db, modRecordQuery.c_str(), 0, 0, &errorMsg);
+    if (rc != SQLITE_OK) {
+        cerr<<"\033[31mSQL error: "<< errorMsg<<"\033[0m"<<endl;
+        sqlite3_free(errorMsg);
+    } else {
+        cout<<"\n\033[33mContact Modified!!\033[0m\n";
+    }
+    // Close the connection
+    sqlite3_close(db);
 }
 
 void search_contact() {
